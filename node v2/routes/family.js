abe3935204a5ec3d7ea3fdeaa3fb7a0c4c25e603
dhong639 <30446369+dhong639@users.noreply.github.com>
2021-05-01 02:32:36 -0500

    Update family.js

--- node v2/routes/family.js
+++ node v2/routes/family.js
@@ -1,32 +1,9 @@
 const express = require('express');
+
 const router = express.Router();
 const client = require('./../database.js');
 
-function buildInsert(listNotNull, listOptional, listReq, listReqValue, fields, values, position) {
-	var counter = 0;
-	for(var i = 0; i<listReq.length; i++) {
-		for(var j = 0; j<listNotNull.length; j++) {
-			if(listReq[i] == listNotNull[j] && listReqValue[i] != '') {
-				fields.push(listReq[i]);
-				values.push(listReqValue[i]);
-				counter += 1;
-				position.push('$' + counter);
-				delete listNotNull[j];
-				continue;
-			}
-		}
-		for(var j = 0; j<listOptional.length; j++) {
-			if(listReq[i] == listOptional[j] && listReqValue[i] != '') {
-				fields.push(listReq[i]);
-				values.push(listReqValue[i]);
-				counter += 1;
-				position.push('$' + counter);
-				delete listOptional[j];
-				continue;
-			}
-		}
-	}
-}
+const buildInsert = require('./../query-builder.js');
 
 router.get('/:user_id([0-9]+)', function(req, res) {
 	var text = 'SELECT * FROM User_Account WHERE user_id = $1';
@@ -43,12 +20,30 @@ router.get('/:user_id([0-9]+)', function(req, res) {
 				header: 'Family Profile: ' + name, 
 				email: queryRes.rows[0].email, 
 				phone: queryRes.rows[0].phone, 
-				insert_link: '/family/' + req.params.user_id + '/page-insert'
+				insert_link: '/family/' + req.params.user_id + '/page-insert', 
+				list_link: '/family/' + req.params.user_id + '/page-list'
 			});
 		})
 		.catch(queryErr => {
 			res.send(queryErr);
 		});
+});
+
+router.get('/:user_id([0-9]+)/page-list', function(req, res) {
+	text = 'SELECT page_name, donation_goal, deadline, status FROM Page_Details WHERE family_id = $1';
+	values = [req.params.user_id];
+	client.query(text, values)
+		.then(queryRes => {
+			//res.send(queryRes);
+			res.render('client-pages.pug', {
+				headers: ['page_name', 'donation_goal', 'deadline', 'status'], 
+				body: queryRes.rows
+			});
+		}) 
+		.catch(queryErr => {
+			res.send(queryErr);
+		});
+	
 });
 
 router.get('/:user_id([0-9]+)/page-insert', function(req, res) {
@@ -59,29 +54,18 @@ router.get('/:user_id([0-9]+)/page-insert', function(req, res) {
 });
 
 router.post('/:user_id([0-9]+)/page-insert', function(req, res) {
-	var listNotNull = [
-		'family_id', 'page_name', 'status', 
-		'donation_goal', 'deadline'];
-	var listOptional = [
-		'visitation_date', 'visitation_time', 'visitation_location', 'visitation_description', 
-		'funeral_date', 'funeral_time', 'funeral_location', 'funeral_description', 
-		'obituary', 'images'];
-	var listReq = [
-		'family_id', 'page_name', 'status', 'visitation_date', 'visitation_time', 'visitation_location', 'visitation_description', 'funeral_date', 'funeral_time', 'funeral_location', 'funeral_description', 
-		'obituary', 'images', 
-		'donation_goal', 'deadline'];
-	var listReqValue = [
-		req.params.user_id, req.body.page_name, 1, 
-		req.body.visitation_date, req.body.visitation_time, req.body.visitation_location, req.body.visitation_description, 
-		req.body.funeral_date, req.body.funeral_time, req.body.funeral_location, req.body.funeral_description, 
-		req.body.obituary, req.body.images, 
-		req.body.donation_goal, req.body.deadline];
-	var fields = [];
-	var values = [];
-	var position = [];
-	buildInsert(listNotNull, listOptional, listReq, listReqValue, fields, values, position);
-	var text = 'INSERT INTO Page_Details (' + fields.join(', ') + ') VALUES (' + position.join(', ') + ')';
-	client.query(text, values)
+	var reqFields = Object.keys(req.body);
+	reqFields.pop();
+	reqFields.unshift('status');
+	reqFields.unshift('family_id');
+
+	var reqValues = Object.values(req.body);
+	reqValues.pop();
+	reqValues.unshift(1);
+	reqValues.unshift(req.params.user_id);
+	
+	var query = buildInsert(reqFields, reqValues, 'Page_Details');
+	client.query(query)
 		.then(queryRes => {
 			res.render('confirm', {
 				message: 'Data submitted successfully', 
