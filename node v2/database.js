d053c27925d3d3a008f2222bd2b4d53883027c58
dhong639 <30446369+dhong639@users.noreply.github.com>
2021-05-12 16:13:02 -0500

    Add files via upload

--- node v2/database.js
+++ node v2/database.js
@@ -1,5 +1,18 @@
+/*
+*	David Hong
+*	ECS 3200
+*	Carson's Village: Automated Family Page
+*	database.js
+*		defines database connection
+*	WARNING: DO NOT DIRECTLY DECLARE CREDENTIALS IN FINAL PRODUCT, PROTOTYPE/DEMONSTRATION USE ONLY
+*		in final product, pass credentials as environment variables for security reasons
+*/
 const {Client} = require('pg');
-
+/*
+*	declare PostgreSQL database credentials to initiate connection
+*	the below represents default user, host, password, and port for a PostgresSQL table
+*		for security reasons, set these to a different value in the final product
+*/
 const client = new Client({
 	user: 'postgres', 
 	host: 'localhost', 
@@ -7,5 +20,5 @@ const client = new Client({
 	password: 'postgres', 
 	port: 5432
 });
-
+//export modules for user in server.js
 module.exports = client;
--- node v2/server.js
+++ node v2/server.js
@@ -1,35 +1,43 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+/*
+*	David Hong
+*	ECS 3200
+*	Carson's Village: Automated Family Page
+*	server.js
+*		Denotes server to route functions
+*		Located under "/"
+*/
+const express = require('express');									//load express for front-end and routes
+const app = express();												//set application as express
+const port = 3000;													//set port to 3000
 
-const client = require('./database.js');
+const client = require('./database.js');							//load database connection
 
-const routeLogin = require('./routes/login.js');
-const routeGeneral = require('./routes/general.js');
-const routeAdvocateAdmin = require('./routes/advocate-admin.js');
-const routeFamily = require('./routes/family.js');
+const routeLogin = require('./routes/login.js');					//load login route
+const routeGeneral = require('./routes/general.js');				//load general route
+const routeAdvocateAdmin = require('./routes/advocate-admin.js');	//load advocate-admin route
+const routeFamily = require('./routes/family.js');					//load family route
 
-app.use(express.urlencoded({extended: false}));
-app.set('view engine', 'pug');
+app.use(express.urlencoded({extended: false}));						//set body-parser
+app.set('view engine', 'pug');										//set view engine to pug
 
-client.connect();
+client.connect();													//connect to database
 
-client.query('SELECT NOW()')
+client.query('SELECT NOW()')										//check database connectivity
 	.then(res => console.log(res.rows[0]))
 	.catch(err => console.error(err.stack));
 
-app.get('/', function(req ,res) {
+app.get('/', function(req ,res) {									//redirect root url to login
 	res.redirect('/login');
 });
 
-app.use('/login', routeLogin);
+app.use('/login', routeLogin);										//route login functions
 
-app.use('/general', routeGeneral);
+app.use('/general', routeGeneral);									//route general functions
 
-app.use('/advocate-admin', routeAdvocateAdmin);
+app.use('/advocate-admin', routeAdvocateAdmin);						//route advocate-admin functions
 
-app.use('/family', routeFamily);
+app.use('/family', routeFamily);									//route family functions
 
-app.listen(port, function() {
+app.listen(port, function() {										//set server to run contiously on port 3000
 	console.log('Listening on port ' + port + '...');
 });
